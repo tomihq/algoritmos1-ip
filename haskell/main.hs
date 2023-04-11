9def6ca96acf74cecc2d3403328d53c8a19f727f
Tomás Hernández <hernandeztomas584@gmail.com>
2023-04-10 22:14:23 -0300

    add: exercises digitoDecenas, prodInt, todoMenor

--- haskell/main.hs
+++ haskell/main.hs
@@ -71,6 +71,22 @@ sumarDistintos n1 n2 n3 | n1 == n2 = n1 + n3
 esMultiploDe :: Integer -> Integer -> Bool
 esMultiploDe n1 n2 = mod n1 n2 == 0
 
---Ejercicio 2.i
+--Ejercicio 2.i (dividir un número por 10 y luego calcular su resto dará el último digito de la unidad) 4235 - 4235 / 10 = 423.5 / mod(423.5) = 5
 digitoUnidades :: Integer -> Integer
 digitoUnidades n1 =  mod n1 10
+
+--Ejercicio 2.j
+digitoDecenas :: Integer -> Integer
+digitoDecenas n1 =  mod n1 100
+
+--Ejercicio 3 -- TODO. Buscar si existe un k que haga la ecuacion = 0 siempre y cuando k no sea 0.
+--estanRelacionados :: Integer -> Integer -> Bool
+--estanRelacionados n1 n2 = (n1 * n1) + (n1 * n2 * k) == 0
+
+--Ejercicio 4.a Producto interno (producto escalar) entre dos tuplas RxR = R2. (1, 2) (3, 4) = 3 * 1 + 2 * 4 = 11. Preguntar como usar los reales en Haskell.
+prodInt :: (Float, Float) -> (Float, Float) -> Float 
+prodInt tuplaUno tuplaDos = (fst tuplaUno * fst tuplaDos) + (snd tuplaUno * snd tuplaDos)
+
+{-Ejercicio 4.b. todoMenor: Decidir si toda coordenada de la primera tupla es menor que a la coordenada de la segunda tupla.-}
+todoMenor :: (Float, Float) -> (Float, Float) -> Bool
+todoMenor t1 t2 = fst t1 < fst t2 && snd t1 < snd t2 
