555db3c4d61532fd0945409bfb4634fad69b1d69
Tomás Hernández <hernandeztomas584@gmail.com>
2023-04-17 20:27:09 -0300

    add: guide 2 - ex 3

--- haskell/main2.hs
+++ haskell/main2.hs
@@ -17,3 +17,15 @@ parteEntera :: Float -> Integer
 parteEntera n | (n>=0 && n<1) || (n > -1 && n<=0) = 0
               | n >= 1 = 1 + parteEntera(n-1)
               | n <= 1 = -1 + parteEntera(n+1)
+
+{- Ejercicio 3. Dados dos numeros naturales determine si el primero es divisile por el segundo. No esta permitido utilizar mod ni div. 
+-- no es divisible -> 9 2 -> 9 - 2 -> 7 - 2 -> 5 - 2 -> 3 - 2 -> 1. Al no ser 0 no es divisible.
+-- no es divisible -> 9 5 -> 9 - 5 -> 4 - 5 -> Como x<y entonces no es divisible.
+-- es divisible -> 10 5 -> 10 - 5 -> 5 - 5 -> Como quedó x-y == 0 entonces es divisible.
+-- es divisible -> 4 1 -> 4 - 1 -> 3 - 1 -> 2 - 1 -> 1 - 1 -> como quedó x-y == 0 entones es divisible.
+-}
+
+esDivisible :: Integer -> Integer -> Bool
+esDivisible x y | (x < y) && (x > 0) = False
+                | (x-y == 0) = True
+                | otherwise = esDivisible (x-y) y
