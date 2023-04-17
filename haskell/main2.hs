fa35ca1240c7b602cb02ebacfcfaee5e3186d1cf
Tomás Hernández <hernandeztomas584@gmail.com>
2023-04-17 20:21:31 -0300

    add: guide 2 - ex 2

--- haskell/main2.hs
+++ haskell/main2.hs
@@ -8,3 +8,12 @@ fib n | n == 0 = 0
 fibonacci :: Integer -> Integer
 fibonacci n = fib n
 
+{-Ejercicio 2. 
+    4.82 -> 3.82 -> 2.82 -> 1.82 -> 0.82 entonces como es menor que 1 sumo 0.
+    -4.5 -> 1 + parteEntera(-4.5 + 1) => 1 + parteEntera(-5.5)
+-}
+
+parteEntera :: Float -> Integer
+parteEntera n | (n>=0 && n<1) || (n > -1 && n<=0) = 0
+              | n >= 1 = 1 + parteEntera(n-1)
+              | n <= 1 = -1 + parteEntera(n+1)
