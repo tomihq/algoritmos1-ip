562979ce525893aea0245253efe43ad6289860ba
Tomás <hernandeztomas584@gmail.com>
2023-05-08 16:27:16 -0300

    feat: testSuite & add: variable to runCases

--- haskell/TestDeMisFunciones.hs
+++ haskell/TestDeMisFunciones.hs
@@ -3,3 +3,9 @@ module TestDeMisFunciones where
 import Test.HUnit
 import MultiplosDeN --carga un archivo.
 
+testSuite = test[
+    "Caso 1: Múltiplos de 2" ~: (multiplosDeN 2 [1, 2, 3, 4, 5, 6]) ~?= [2, 4, 6]
+    ]
+
+runCases = runTestTT testSuite
+--Para probarlo en GHCI colocar runCases
