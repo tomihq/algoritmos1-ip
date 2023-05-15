38cfee418e8911cf361dc553c9bc70eec8fd5520
Tomas <hernandeztomas584@gmail.com>
2023-05-15 14:50:42 -0300

    add: prueba.py

---
+++ python/prueba.py
@@ -0,0 +1,7 @@
+#me paro en directorio del archivo, python3, import nombreArchivo, luego nombreArchivo.funcion(parametro)
+#cada vez que haga un cambio debo importar nuevamente el archivo.
+def doble(n: int):
+    resultado: int = 2 * n
+    print(resultado)
+    return resultado
+
